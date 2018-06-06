bba0f66937985ecb6e08e2fd12bb3a117da10d23
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-06-05 21:59:27 -0500

    JSLT and Custome tag demo

---
+++ ForEachDemo/src/qnguyen/Head.java
@@ -0,0 +1,38 @@
+package qnguyen;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class Head extends SimpleTagSupport {
+    String thecolor;
+    String words;
+
+    //This is called from JSP servlet to render custom tag
+    public void doTag() throws JspException, IOException {
+        JspWriter out = getJspContext().getOut();
+        if (thecolor != null)
+            out.write(String.format("<span style='color:%s'>%s</span>", thecolor, words));
+        else
+            out.write(String.format("<span>%s</span>", words));
+    }
+
+    //Need a setter for each attribute of custom tag
+
+    public String getThecolor() {
+        return thecolor;
+    }
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setThecolor(String thecolor) {
+        this.thecolor = thecolor;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+}
---
+++ ForEachDemo/web/label.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix="mytag" uri="/WEB-INF/tld/TldDemo" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: 986375
+  Date: 6/5/2018
+  Time: 1:38 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>JSTL and Custom Tags Lecture</title>
+</head>
+<body>
+<mytag:Head thecolor='red' words='hello'/>
+</body>
+</html>
