2625f1e7d77af740caf5b1284a9646506ea37e60
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-06-13 14:23:09 -0500

    wrong code

--- Javascript and ASCII Animation/web/ascii.js
+++ Javascript and ASCII Animation/web/ascii.js
@@ -1,48 +1,72 @@
-var duration = 250;
-var objInterval;
-var currentFrame = 0;
-var isStart = false;
-$("#startBtn").click(function () {
+"use strict";
+const NORMAL_SPEED = 250;
+const TURBO_SPEED = 50;
+let duration = NORMAL_SPEED;
+let objInterval;
+let currentFrame = 0;
+let presetAnimation;
+
+/**
+ * start animation event
+ */
+function onStart() {
+    if (presetAnimation !== "changeanimation") {
+        presetAnimation = document.getElementById('mytextarea').value;
+    }
     objInterval = setInterval(generateAnimation, duration);
-    isStart = true;
-    $("#stopBtn").prop('disabled', !isStart);
-    $("#aminationopt").prop('disabled', isStart);
-    $("#isStart").prop('disabled', !isStart);
-});
-
-$("#stopBtn").click(function () {
-    clearInterval(objInterval);
-    isStart = false;
-    $("#isStart").prop('disabled', isStart);
-    $("#stopBtn").prop('disabled', !isStart);
-    $("#aminationopt").prop('disabled', isStart);
-})
-
-function generateAnimation() {
-    var abc = $('#aminationopt').find(":selected").text();
-    var arrAnimation = ANIMATIONS[abc].split("=====\n");
-    if (currentFrame >= arrAnimation.length)
-        currentFrame = 0;
-    $('#mytextarea').val(arrAnimation[currentFrame++]);
+    preAnimation(true);
 }
 
-$('#aminationopt').on('change', function () {
-    // alert("123121");
+/**
+ * generate animation from text.
+ */
+function generateAnimation() {
+    let e = document.getElementById("aminationopt");
+    let abc = e.options[e.selectedIndex].value;
+    let arrAnimation = ANIMATIONS[abc].split("=====\n");
+    if (presetAnimation !== null && presetAnimation !== "changeanimation") {
+        arrAnimation.unshift(presetAnimation);
+    }
+    if (currentFrame >= arrAnimation.length) {
+        currentFrame = 0;
+    }
+    document.getElementById('mytextarea').value = arrAnimation[currentFrame++];
+}
 
-});
-$('#sizeopt').on('change', function () {
-    $("#mytextarea").css({'font-size': $(this).val()});
-});
+/**
+ * disable/enable UI
+ * @param {boolean} isStart The second number.
+ */
+function preAnimation(isStart) {
+    document.getElementById('startBtn').disabled = isStart;
+    document.getElementById('aminationopt').disabled = isStart;
+    document.getElementById('stopBtn').disabled = !isStart;
+}
 
-$("#speedcheckbox").change(function () {
-    duration = this.checked ? 50 : 250
+/**
+ * stop button click event
+ */
+function onStop() {
+    preAnimation(false);
     clearInterval(objInterval);
-    objInterval = setInterval(generateAnimation, duration)
-});
-//
-// $("#aminationopt").change(function() {
-//    alert($(this).val());
-// });
-//
-// var conceptName = $('#aminationopt').find(":selected").text();
-// alert(conceptName);
+    if (presetAnimation !== null && presetAnimation !== "" && presetAnimation !== "changeanimation") {
+        document.getElementById('mytextarea').value = presetAnimation;
+    }
+}
+
+/**
+ * change size event
+ */
+function onChangeSize() {
+    document.getElementById("mytextarea").style.fontSize = document.getElementById("sizeopt").value;
+}
+
+/**
+ * stop button click event
+ */
+function onCheckSpeed() {
+    let checkedValue = document.getElementById("speedcheckbox").checked;
+    duration = checkedValue ? TURBO_SPEED : NORMAL_SPEED;
+    clearInterval(objInterval);
+    objInterval = setInterval(generateAnimation, duration);
+}
