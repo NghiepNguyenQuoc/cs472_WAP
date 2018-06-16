08053c7ab62c8a5ecb6b6ceefae92fca4e8245f2
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-06-16 00:20:12 -0500

    modified script

--- DOM and Events15 Puzzle/web/fifteen.js
+++ DOM and Events15 Puzzle/web/fifteen.js
@@ -1,12 +1,18 @@
+var empty_row = 3;
+var empty_col = 3;
+const PUZZLE_NUMBER = 4;
+const TILE_SIZE = 100;
 $(document).ready(function () {
     $('#puzzlearea div').each(function (i) {
         // calculate x and y for this piece
-        var x = ((i % 4) * 100);
-        var y = (Math.floor(i / 4) * 100);
+        var row = i % PUZZLE_NUMBER;
+        var col = Math.floor(i / PUZZLE_NUMBER);
+        var x = (row * TILE_SIZE);
+        var y = (col * TILE_SIZE);
 
         // set basic style and background
         this.className = "puzzlepiece";
-        this.id= "puzzlepiece";
+        this.id = "square_" + row + "_" + col;
         this.style.left = x + 'px';
         this.style.top = y + 'px';
         this.style.backgroundImage = 'url("background.jpg")';
@@ -15,5 +21,8 @@ $(document).ready(function () {
         // store x and y for later
         this.x = x;
         this.y = y;
+        $(this).click(function () {
+            
+        })
     });
 });
